16bf9a6052aefd8334d6ca4a4696da84725bdeb0
qiang <qw13131wang@gmail.com>
2023-01-07 13:52:57 +0800

    chore: update README (#23)

--- README.md
+++ README.md
@@ -1,4 +1,117 @@
-# Element Plus Nuxt [WIP]
+# Element Plus Nuxt
+
+> [Element Plus](https://element-plus.org) module for [Nuxt](https://nuxt.com)
+
+## Features
+
+- Automatically import components and styles on demand.
+- Automatically import directives and styles on demand.
+- Automatically import icons from [@element-plus/icons-vue](https://github.com/element-plus/element-plus-icons).
+- Automatically import of ElMessage, ElNotification and other methods.
+- Automatically inject the ID_INJECTION_KEY into Vue.
+- Automatically inject the teleport markup into the correct location in the final page HTML.
+
+## Installation
+
+```bash
+npm i @element-plus/nuxt -D
+```
+
+## Usage
+
+```ts
+export default defineNuxtConfig({
+  modules: [
+    '@element-plus/nuxt'
+  ],
+  elementPlus: { /** Options */ }
+})
+```
+
+## Options
+
+### importStyle
+
+- Type: `'css' | 'scss'`
+- Default: `css`
+
+import style css or sass(scss) with components.
+
+### themes
+
+- Type: `array`
+
+A list of themes that require import styles automatically.
+
+e.g. `['dark']`
+
+### icon
+
+- Type: `string | false`
+- Default: `ElIcon`
+
+Icon prefix name, disable automatically import icon with `false`.
+
+### namespace
+
+- Type: `string`
+- Default: `el`
+
+When you change the global namespace, you must change it here as well.
+
+### injectionID
+
+- Type: `object`
+
+Automatically inject the ID_INJECTION_KEY into Vue.
+
+### appendTo
+
+- Type: `array`
+
+When you modify the `append-to` props in all based on ElTooltip components, you need to add the value here.
+
+### components
+
+- Type: `array`
+
+If there are components that are not imported automatically from Element Plus, you need to add the component name here.
+
+e.g. `['ElSubMenu']`
+
+### directives
+
+- Type: `object`
+
+If there are directives that are not imported automatically from Element Plus, you need to add the directive name here.
+
+### imports
+
+- Type: `array`
+
+If you wish to add automatically import content from Element Plus, you can add it here.
+
+### noStylesComponents
+
+- Type: `array`
+
+When a component incorrectly loads styles, you need to add the component name here.
+
+### include
+
+- Type: `array`
+
+Include files that need to automatically import styles.
+
+### exclude
+
+- Type: `array`
+
+Exclude files that do not require the automatic import of styles.
+
+## Template
+
+[element-plus-nuxt-starter](https://github.com/element-plus/element-plus-nuxt-starter)
 
 ## Development
 
@@ -7,4 +120,4 @@
 - Run `pnpm dev` to start playground in development mode.
 - Run `pnpm dev:build` to build playground.
 - Run `pnpm dev:start` to locally preview playground.
-- Use `pnpm build` to build this project.
+- Run `pnpm build` to build this project.
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 import type { ElIdInjectionContext } from 'element-plus'
 
-/** name: export name from library, as: name you want to use in your project, from: name of library */
+/** name: export name from the library, as: the name you want to use in your project, from: the name of library */
 export type PresetImport = string | [name: string, as?: string, from?: string]
 
-/** directive: export name from library, name: export name with style */
+/** directive: export name from the library, name: export name with style */
 export type PresetDirectives = Record<string, string | [directive: string, name?: string]>
 
 /** Used to filter files that need to automatically import styles and other functions */
@@ -18,7 +18,7 @@ export interface Options extends TransformOptions {
    *
    * @default 'from element-plus/es/component'
    *
-   * When some components are not automatically imported from Element Plus, you need to add the component name to here.
+   * If there are components that are not imported automatically from Element Plus, you need to add the component name here.
    *
    * @example
    * ```ts
@@ -47,7 +47,7 @@ export interface Options extends TransformOptions {
    *  ['ElLoading', 'ElMessage', 'ElMessageBox', 'ElNotification']
    * ```
    *
-   * When you need to add automatic import content from Element Plus, you can add it to here.
+   * When you need to add automatically import content from Element Plus, you can add it here.
    *
    * @example
    * ```ts
@@ -73,9 +73,9 @@ export interface Options extends TransformOptions {
    */
   importStyle: 'css' | 'scss'
   /**
-   * Themes that need to be automatically imported from Element Plus.
+   * A list of themes that require importing styles automatically.
    *
-   * Currently, only [dark](https://element-plus.org/en-US/guide/dark-mode.html) are supported .
+   * Currently, only [dark](https://element-plus.org/en-US/guide/dark-mode.html) is supported.
    *
    * @example
    * ```ts
@@ -84,7 +84,7 @@ export interface Options extends TransformOptions {
    */
   themes: Array<'dark'>
   /**
-   * a list of component names that have no styles, so resolving their styles file should be prevented
+   * A list of component names that have no styles, so resolving their styles file should be prevented
    *
    * @default
    * ```ts
@@ -104,7 +104,7 @@ export interface Options extends TransformOptions {
   /**
    * Global component className prefix.
    *
-   * When you modify the global namespace, you need to modify it here at the same time.
+   * When you change the global namespace, you must change it here as well.
    *
    * @default 'el'
    */
@@ -112,19 +112,19 @@ export interface Options extends TransformOptions {
   /**
    * Which element the tooltip CONTENT appends to.
    *
-   * When you modify the `append-to` props in all based on Tooltip components, you need to add the relevant values here, so as to avoid hydrate errors.
+   * When you modify the `append-to` props in all based on ElTooltip components, you need to add the value here, to avoid hydrate errors.
    *
-   * If you used `Teleport` to teleport a part of a component's template into a DOM node that near the `<body>` tag, you can also add this ID here. The internal plug-in will automatically handle hydrate errors.
+   * If you used `Teleport` to teleport a part of a component's template into a DOM node near the `<body>` tag, you can also add this ID here. The internal plug-in will automatically handle hydrate errors.
    */
   appendTo: string[]
   /**
-   * Prefix name of the icon.
+   * Icon prefix name.
    *
    * To avoid the duplication of icon names with native DOM or other components, we recommend adding a prefix to the icon.
    *
    * @default 'ElIcon'
    *
-   * Disable automatic imports of icons by configuring `false`.
+   * Disable automatically import icon with `false`
    */
   icon: false | string
 }
