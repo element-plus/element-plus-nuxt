c67a6e44a6adb0e88d71892a4909698615e3b810
Horu <73709188+HigherOrderLogic@users.noreply.github.com>
2023-01-28 09:36:44 +0700

    fix: transpile `element-plus` on build (#30)
    * fix: transpile `element-plus`
    
    * chore: moving stuff
    
    * chore: chores

--- src/core/nuxtOptions.ts
+++ src/core/nuxtOptions.ts
@@ -3,5 +3,5 @@ import { useNuxt } from '@nuxt/kit'
 export function resolveOptions () {
   const nuxt = useNuxt()
 
-  nuxt.options.build.transpile.push('element-plus/es')
+  nuxt.options.build.transpile.push('element-plus')
 }
