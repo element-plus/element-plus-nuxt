b8771008132ec2a09ba6e29b1090044dd9620174
tolking <qw13131wang@gmail.com>
2024-03-23 12:13:36 +0800

    feat: add dayjs to optimizeDeps

--- src/core/options.ts
+++ src/core/options.ts
@@ -3,6 +3,14 @@ import { libraryName } from '../config'
 
 export function resolveOptions () {
   const nuxt = useNuxt()
+  const optimizeDeps = nuxt.options.vite.optimizeDeps?.include ?? []
 
   nuxt.options.build.transpile.push(libraryName)
+  if (nuxt.options.vite.optimizeDeps) {
+    nuxt.options.vite.optimizeDeps.include = [...optimizeDeps, 'dayjs', 'dayjs/plugin/*']
+  } else {
+    nuxt.options.vite.optimizeDeps = {
+      include: ['dayjs', 'dayjs/plugin/*']
+    }
+  }
 }
