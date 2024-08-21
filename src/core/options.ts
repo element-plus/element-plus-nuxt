ef7b227a0a9caa2fa6f13f9631efe65ab9a54f8f
qiang <qw13131wang@gmail.com>
2024-08-21 14:10:59 +0800

    fix: reset transpile config

--- src/core/options.ts
+++ src/core/options.ts
@@ -3,12 +3,10 @@ import { libraryName } from '../config'
 
 export function resolveOptions () {
   const nuxt = useNuxt()
-  const styleRegExp = new RegExp(`${libraryName}/es/components/.*/style/(index|css).mjs`)
-  const themeRegExp = new RegExp(`${libraryName}/theme-chalk/.*`)
   // Is not an ES Module dependency
   const include = ['dayjs', 'dayjs/plugin/*.js']
 
-  nuxt.options.build.transpile.push(styleRegExp, themeRegExp)
+  nuxt.options.build.transpile.push(libraryName)
   nuxt.options.vite.optimizeDeps ||= {}
   nuxt.options.vite.optimizeDeps.include ||= []
   nuxt.options.vite.optimizeDeps.include.push(...include)
