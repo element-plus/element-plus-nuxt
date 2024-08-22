094fb90fb6e231012e48ef85c2c7751fb249666d
qiang <qw13131wang@gmail.com>
2024-08-22 10:57:43 +0800

    chore: add lodash-unified to optimizeDeps

--- src/core/options.ts
+++ src/core/options.ts
@@ -1,13 +1,11 @@
 import { useNuxt } from '@nuxt/kit'
-import { libraryName } from '../config'
+import { libraryName, optimizeDeps } from '../config'
 
 export function resolveOptions () {
   const nuxt = useNuxt()
-  // Is not an ES Module dependency
-  const include = ['dayjs', 'dayjs/plugin/*.js']
 
   nuxt.options.build.transpile.push(libraryName)
   nuxt.options.vite.optimizeDeps ||= {}
   nuxt.options.vite.optimizeDeps.include ||= []
-  nuxt.options.vite.optimizeDeps.include.push(...include)
+  nuxt.options.vite.optimizeDeps.include.push(...optimizeDeps)
 }
