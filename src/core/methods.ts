6f1fe6e7014facb458f47afa158c50906b7908a4
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-12 01:14:57 +0200

    chore: tree shaking attempt

--- src/core/methods.ts
+++ src/core/methods.ts
@@ -4,11 +4,38 @@ export function resolveMethods () {
   return {
     filename: `${libraryName}-methods.plugin.mjs`,
     getContents: () => {
-      return `import { defineNuxtPlugin, ElMessage, ElMessageBox, ElNotification, ElLoading } from '#imports';
+      return `
+import { defineNuxtPlugin, useState } from '#imports'
 
-export default defineNuxtPlugin(nuxtApp => {
-  for (const m of [ElMessage, ElMessageBox, ElNotification, ElLoading])
-    nuxtApp.vueApp.use(m)
+function camelize(value) {
+  return value.replace(/(^|-)(\\w)/g, (a, b, c) => c.toUpperCase())
+}
+
+const allImportsWithStyle = [
+  'ElLoading',
+  'ElMessage',
+  'ElMessageBox',
+  'ElNotification',
+]
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const methodComps = useState('element-plus', () => [])
+
+  nuxtApp.hook('app:rendered', (ctx) => {
+    const allComps = [
+      ...(ctx.renderResult?.renderStyles()?.matchAll(/el-\\w+\\.css/g) ?? []),
+    ].flatMap(([comp]) => comp)
+    methodComps.value = [...new Set(allComps)]
+      .map((comp) => camelize(comp.substring(0, comp.length - 4)))
+      .filter((comp) => allImportsWithStyle.includes(comp))
+  })
+
+  nuxtApp.hook('app:beforeMount', async (app) => {
+    const plugins = await import('element-plus').then((lib) =>
+      methodComps.value.map((comp) => lib[comp]),
+    )
+    for (const plug of plugins) app.use(plug)
+  })
 })
 `
     }
