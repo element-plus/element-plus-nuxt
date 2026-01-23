4bc722be6b350ccb344fe2accbd94c01aa8ce580
qiang <qw13131wang@gmail.com>
2026-01-23 14:58:24 +0800

    fix: cannot be used in the moduleDependencies

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
-import { addPluginTemplate, addTemplate, defineNuxtModule } from '@nuxt/kit'
-import { defaults, libraryName } from './config'
+import { addPluginTemplate, addTemplate, defineNuxtModule } from "@nuxt/kit";
+import { defaults, libraryName } from "./config";
 import {
   resolveCache,
   resolveComponents,
@@ -15,88 +15,102 @@ import {
   resolveThemes,
   resolveMethods,
   transformPlugin,
-  localePlugin
-} from './core/index'
-import { getLayersDir } from './utils'
-import type { ModuleOptions } from './types'
-export type { ModuleOptions } from './types'
+  localePlugin,
+} from "./core/index";
+import { getLayersDir } from "./utils";
+import type { ModuleOptions } from "./types";
+export type { ModuleOptions } from "./types";
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: libraryName,
-    configKey: 'elementPlus',
+    configKey: "elementPlus",
     compatibility: {
-      nuxt: '>=3'
-    }
+      nuxt: ">=3",
+    },
   },
   defaults,
-  setup (options, nuxt) {
-    const layers = getLayersDir(nuxt.options._layers)
+  onInstall() {
+    resolveBaseImports({} as ModuleOptions);
+  },
+  setup(options, nuxt) {
+    const layers = getLayersDir(nuxt.options._layers);
 
     // disable the `cache` option when building applications
     if (!nuxt.options.dev) {
-      options.cache = false
+      options.cache = false;
     }
 
-    resolveOptions(options)
-    resolveThemes(options)
-    resolveBaseImports(options)
-    nuxt.options.imports.autoImport !== false && resolveImports(options)
-    nuxt.options.components !== false && resolveComponents(options)
-    options.cache && addTemplate(resolveCache(options))
-    options.globalConfig && addPluginTemplate(resolveGlobalConfig(options))
-    options.importStyle === 'scss' && options.themeChalk && resolveThemeChalk(options).map(addTemplate)
+    resolveOptions(options);
+    resolveThemes(options);
+    resolveBaseImports(options);
+    nuxt.options.imports.autoImport !== false && resolveImports(options);
+    nuxt.options.components !== false && resolveComponents(options);
+    options.cache && addTemplate(resolveCache(options));
+    options.globalConfig && addPluginTemplate(resolveGlobalConfig(options));
+    options.importStyle === "scss" &&
+      options.themeChalk &&
+      resolveThemeChalk(options).map(addTemplate);
 
     if (nuxt.options.ssr !== false) {
-      addPluginTemplate(resolveInjection(options))
-      addPluginTemplate(resolveTeleports(options))
-      options.installMethods.length && addPluginTemplate(resolveMethods(options))
+      addPluginTemplate(resolveInjection(options));
+      addPluginTemplate(resolveTeleports(options));
+      options.installMethods.length &&
+        addPluginTemplate(resolveMethods(options));
     }
 
-    nuxt.hook('vite:extendConfig', (config, { isClient }) => {
-      const mode = isClient ? 'client' : 'server'
+    nuxt.hook("vite:extendConfig", (config, { isClient }) => {
+      const mode = isClient ? "client" : "server";
 
-      config.plugins = config.plugins || []
-      config.plugins.push(transformPlugin.vite({
-        layers,
-        cache: options.cache,
-        include: options.include,
-        exclude: options.exclude,
-        sourcemap: nuxt.options.sourcemap[mode],
-        transformStyles: name => resolveStyles(options, name),
-        transformDirectives: name => resolveDirectives(options, name)
-      }))
-
-      if (options.defaultLocale !== 'en') {
-        config.plugins.push(localePlugin.vite({
-          sourcemap: nuxt.options.sourcemap[mode],
-          locale: options.defaultLocale
-        }))
-      }
-    })
-
-    nuxt.hook('webpack:config', (configs) => {
-      configs.forEach((config) => {
-        const mode = config.name === 'client' ? 'client' : 'server'
-
-        config.plugins = config.plugins || []
-        config.plugins.push(transformPlugin.webpack({
+      config.plugins = config.plugins || [];
+      config.plugins.push(
+        transformPlugin.vite({
           layers,
           cache: options.cache,
           include: options.include,
           exclude: options.exclude,
           sourcemap: nuxt.options.sourcemap[mode],
-          transformStyles: name => resolveStyles(options, name),
-          transformDirectives: name => resolveDirectives(options, name)
-        }))
+          transformStyles: (name) => resolveStyles(options, name),
+          transformDirectives: (name) => resolveDirectives(options, name),
+        }),
+      );
 
-        if (options.defaultLocale !== 'en') {
-          config.plugins.push(localePlugin.webpack({
+      if (options.defaultLocale !== "en") {
+        config.plugins.push(
+          localePlugin.vite({
             sourcemap: nuxt.options.sourcemap[mode],
-            locale: options.defaultLocale
-          }))
+            locale: options.defaultLocale,
+          }),
+        );
+      }
+    });
+
+    nuxt.hook("webpack:config", (configs) => {
+      configs.forEach((config) => {
+        const mode = config.name === "client" ? "client" : "server";
+
+        config.plugins = config.plugins || [];
+        config.plugins.push(
+          transformPlugin.webpack({
+            layers,
+            cache: options.cache,
+            include: options.include,
+            exclude: options.exclude,
+            sourcemap: nuxt.options.sourcemap[mode],
+            transformStyles: (name) => resolveStyles(options, name),
+            transformDirectives: (name) => resolveDirectives(options, name),
+          }),
+        );
+
+        if (options.defaultLocale !== "en") {
+          config.plugins.push(
+            localePlugin.webpack({
+              sourcemap: nuxt.options.sourcemap[mode],
+              locale: options.defaultLocale,
+            }),
+          );
         }
-      })
-    })
-  }
-})
+      });
+    });
+  },
+});
